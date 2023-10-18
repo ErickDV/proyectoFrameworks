f3fa4b8d41dd3d3222d8db7cd268208e7cefebd6
Carlos Zahid <zahidhdez@outlook.com>
2023-10-18 13:49:24 -0600

    se modifico login.js en el sistema-gestor

--- sistema-gestor/src/Login.js
+++ sistema-gestor/src/Login.js
@@ -1,11 +1,55 @@
+import {useRef, useState, useEffect} from 'react';
 
 const Login = () => {
+
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrmsg] = useState('');
+    const [success, setSuccess] = useState(false);
+    //se enfoca en la entrada del usuario
+    useEffect(()=>{
+        userRef.current.focus();
+    },[])
+
+    //limpiar los mensajes de errores si el usuario o la contrasena cambia
+    useEffect(()=>{
+        setErrmsg('');
+    },[user,pwd])
+
     return(
         <section className="contenedor">
-            <label htmlFor="expediente" >Expediente</label>
-            <input id='expediente'></input>
-            <label htmlFor="password" >Contraseña</label>
-            <input id='password' type='password'></input>
+            <h1>Inicio de sesion</h1>
+            <form className='contenedor'>
+                <label htmlFor="expediente" >Expediente</label>
+                <input  
+                    id='expediente' 
+                    ref={userRef} 
+                    autoComplete='off' 
+                    //funcion anonima
+                    onChange={(e) => setUser(e.target.value)}
+                    //para limpiar entradas despues de el submit
+                    value={user}
+                    required
+                    >
+                    </input>
+                <label htmlFor="password" >Contraseña</label>
+                <input
+                    type='password'  
+                    id='password' 
+                    ref={userRef} 
+                    autoComplete='off' 
+                    //funcion anonima
+                    onChange={(e) => setUser(e.target.value)}
+                    //para limpiar entradas despues de el submit
+                    value={user}
+                    required
+                    >
+                    </input>
+               
+            </form>
         </section>
     )
 }
